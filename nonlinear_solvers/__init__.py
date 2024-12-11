e84bec42ddf5aae023e8d41d269575389f8d60f2
gavinconran <gtconran@gmail.com>
2024-12-11 16:19:07 +0000

    package-install: exit with code 127

--- nonlinear_solvers/__init__.py
+++ nonlinear_solvers/__init__.py
@@ -1,3 +1,3 @@
-"""Contains the nonlinear_solvers __init__ file Ex 5 of OOP4Maths."""
+"""Contains the nonlinear_solvers __init__ file Ex 6 of OOP4Maths."""
 from nonlinear_solvers.solvers import newton_raphson, bisection # noqa F401
 from nonlinear_solvers.solvers import solve, ConvergenceError # noqa F401
