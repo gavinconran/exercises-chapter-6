d00e4ce7002d11b8e15a321f9e2fa8289f00e042
gavinconran <gtconran@gmail.com>
2024-12-11 18:31:52 +0000

    missing , in __init__

--- nonlinear_solvers/__init__.py
+++ nonlinear_solvers/__init__.py
@@ -1,3 +1,3 @@
 """Contains the nonlinear_solvers __init__ file Ex 6 of OOP4Maths."""
-from nonlinear_solvers.solvers import newton_raphson, bisection \
+from nonlinear_solvers.solvers import newton_raphson, bisection, \
                 solve, ConvergenceError # noqa F401
