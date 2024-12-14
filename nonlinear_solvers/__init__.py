d8f4cfe9be1081b51dcd0c605f6074ea56265b95
gavinconran <gtconran@gmail.com>
2024-12-14 16:27:22 +0000

    removed unnecessary comment in __init__.py

--- nonlinear_solvers/__init__.py
+++ nonlinear_solvers/__init__.py
@@ -1,5 +1,3 @@
 """Contains the nonlinear_solvers __init__ file Ex 6 of OOP4Maths."""
-# from nonlinear_solvers.solvers import newton_raphson, bisection, \
-#                solve, ConvergenceError # noqa F401
 from .solvers import newton_raphson, bisection, \
                 solve, ConvergenceError # noqa F401
