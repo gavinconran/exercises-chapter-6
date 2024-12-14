7c35bed34ec171270c647534f3b6dbb64ff4f8b7
gavinconran <gtconran@gmail.com>
2024-12-14 16:24:10 +0000

    relative import in __init__.py

--- nonlinear_solvers/__init__.py
+++ nonlinear_solvers/__init__.py
@@ -1,3 +1,5 @@
 """Contains the nonlinear_solvers __init__ file Ex 6 of OOP4Maths."""
-from nonlinear_solvers.solvers import newton_raphson, bisection, \
+# from nonlinear_solvers.solvers import newton_raphson, bisection, \
+#                solve, ConvergenceError # noqa F401
+from .solvers import newton_raphson, bisection, \
                 solve, ConvergenceError # noqa F401
